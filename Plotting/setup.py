1c94b2aeb0b853407873ec3220efb574917b7e2b
Rhys Doyle <rhys.doyle45@mail.dcu.ie>
2020-01-16 15:12:20 +0000

    Add setup.py

---
+++ Plotting/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='morris-plot',
+    version='1.0',
+    packages=['.'],
+    url='',
+    license='',
+    author='Rhys Doyle',
+    author_email='',
+    description=''
+)
